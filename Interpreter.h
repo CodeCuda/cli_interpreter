2c1cf5c3440737744bc75e13a2b013ee3610a90e
CodeCuda <46074112+CodeCuda@users.noreply.github.com>
2019-06-18 22:18:42 +0300

    Update Interpreter.h

--- Interpreter.h
+++ Interpreter.h
@@ -4,7 +4,9 @@
 #include <map>
 
 
+/*enum TokenType is necessary to determine type of our tokens*/
  enum TokenType { ADD, MULT, SUB, CONCAT, INT, STRING, IDENT, PAR_LEFT, PAR_RIGHT, MINUS, TERMINATOR };
+/*We get tokens from deterministic finite automaton, so we need Input states and Result states*/
  enum Statement{ S0, S1, S2, S3, S4, S5, S6, S7, S11 };
  enum Output { O0, O1, O2, O3, O4, O5, O6, O11 };
 
@@ -17,7 +19,7 @@ using Item = std::pair<Input, Result>;
 
 
 void InitMap();
-
+/*Token class*/
 class Token {
 public:
 	Token(TokenType token_type, std::string token_value) : type(token_type), value(token_value) {};
@@ -34,7 +36,7 @@ private:
 	std::string value;
 };
 
-
+/*our main class*/
 class Interpreter {
 public:
 	Interpreter() {};
@@ -46,8 +48,7 @@ public:
 	int Mult();
 	int Sub();
 	std::string Concat();
-
-//private:
+	
 	std::vector<Token> lexems;
 	std::string::iterator position;
 };
